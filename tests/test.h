fb131be992277bd4e36e8cbcb2650588f1ee831e
Paolo Bonzini <pbonzini@redhat.com>
2012-10-23 09:39:12 +0200

    add tests

---
+++ tests/test.h
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include "debug.h"
+
+#define todo_wine if(0)
+
+#define ok(cond, desc, ...) _ok(cond, #cond, desc, ## __VA_ARGS__ )
+
+static inline void _ok(bool cond, const char *cond_str, const char *str, ...)
+{
+    va_list ap;
+    va_start(ap, str);
+    if (!cond) {
+        printf("FAIL: %s\n  ", cond_str);
+        vprintf(str, ap);
+    } else {
+        printf("ok: %s\n", cond_str);
+    }
+    va_end(ap);
+}
